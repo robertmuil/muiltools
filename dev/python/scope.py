607d1a5cd627b1aacc8acf0780203bbfafb0dc7c
Robert Muil <robert.muil@zalando.de>
2015-06-04 15:56:40 +0200

    stuff

--- dev/python/scope.py
+++ dev/python/scope.py
@@ -3,6 +3,14 @@ yee=1
 bla=[1,2,3]
 ble=np.array([1,2,3])
 
+def inochange():
+	yee=-1
+
+def ichange():
+	global yee
+	print 'local yee='+str(yee)
+	yee=-2
+
 def say_hello(yee=None, bla=None, ble=None):
 	print 'local scope: yee='+str(yee)
 	print 'local scope: bla='+str(bla)
@@ -13,11 +21,26 @@ def say_hello(yee=None, bla=None, ble=None):
 	print 'local scope: bla='+str(bla)
 	print 'local scope: ble='+str(ble)
 
-
 print 'global scope: yee='+str(yee)
 print 'global scope: bla='+str(bla)
 print 'global scope: ble='+str(ble)
+print 'say_hello()...',
 say_hello(yee,bla)
+print '...done'
+print 'global scope: yee='+str(yee)
+print 'global scope: bla='+str(bla)
+print 'global scope: ble='+str(ble)
+print 'inochange()...',
+inochange()
+print '...done'
+print 'global scope: yee='+str(yee)
+print 'global scope: bla='+str(bla)
+print 'global scope: ble='+str(ble)
+yee=2
+print 'global scope: yee='+str(yee)
+print 'ichange()...',
+ichange()
+print '...done'
 print 'global scope: yee='+str(yee)
 print 'global scope: bla='+str(bla)
 print 'global scope: ble='+str(ble)
--- dev/python/test_multiprocessing.py
+++ dev/python/test_multiprocessing.py
@@ -1,12 +1,21 @@
-from multiprocessing import Process, Queue, Pool
+from multiprocessing import Process, Queue, Pool, Array, Value
+import numpy as np
 import time
 import itertools
 
 def f(name, q):
 		print 'hello', name
-		res=f2(99999)
+		res=f2(99999, 10)
 		q.put([name,res])
 		print 'bye ', name
+
+def fg(name, a, st, en):
+	print 'hello {}: {:d} to {:d}'.format(name, st, en)
+	for ii in xrange(st, en):
+		res=f2(99999, ii)
+		a[ii] = res
+	print 'bye ', name
+
 
 def f2(numiter,power):
 		res=0
@@ -30,50 +39,64 @@ def p_f2(numiter):
 		return f2(numiter,power=20)
 
 if __name__ == '__main__':
-		if False:
-				q = Queue()
-				p = Process(target=f, args=('bob',q))
-				p2 = Process(target=f, args=('tom',q))
-				p3 = Process(target=f, args=('dick',q))
-				p.start()
-				p2.start()
-				p3.start()
-				p.join()
-				p2.join()
-				p3.join()
+	test_type=['queue', 'pool', 'array'][2]
+	if test_type == 'queue':
+		q = Queue()
+		p = Process(target=f, args=('bob',q))
+		p2 = Process(target=f, args=('tom',q))
+		p3 = Process(target=f, args=('dick',q))
+		p.start()
+		p2.start()
+		p3.start()
+		p.join()
+		p2.join()
+		p3.join()
 
-				print 'finished'
-				print q.get()
-				print q.get()
-				print q.get()
-				print ''
+		print 'finished'
+		print q.get()
+		print q.get()
+		print q.get()
+		print ''
+	if test_type == 'array':
+		results_array = Array('L', 10)
+		p1 = Process(target=fg, args=('bob',results_array,0,3))
+		p2 = Process(target=fg, args=('tom',results_array,3,7))
+		p3 = Process(target=fg, args=('dck',results_array,7,8))
+		p1.start()
+		p2.start()
+		p3.start()
+		p1.join()
+		p2.join()
+		p3.join()
 
-		if True:
-				print 'start pool...'
-				start_time = time.time()
-				pool = Pool() 
-				a=1000000
-				aa=range(a,a+10)
-				bb=range(10,20)
-				cc = [
-						'tom',
-						'dick',
-						'harry',
-						'spleen',
-						'rinkle',
-						'six',
-						'seven',
-						'eight',
-						'nine',
-						'ten']
-				results=pool.map(f_star, itertools.izip(aa, bb, cc))
-				end_time = time.time()
+		print 'finished'
+		print results_array[:]
+	if test_type == 'pool':
+		print 'start pool...'
+		start_time = time.time()
+		pool = Pool() 
+		a=1000000
+		aa=range(a,a+10)
+		bb=range(10,20)
+		cc = [
+				'tom',
+				'dick',
+				'harry',
+				'spleen',
+				'rinkle',
+				'six',
+				'seven',
+				'eight',
+				'nine',
+				'ten']
+		results=pool.map(f_star, itertools.izip(aa, bb, cc))
+		end_time = time.time()
 
-				print 'took %.2f secs, results:' % (end_time-start_time)
-				for (a,r) in zip(aa,results):
-						print '%d: %s: %d' % (a, r[1], r[0])
+		print 'took %.2f secs, results:' % (end_time-start_time)
+		for (a,r) in zip(aa,results):
+				print '%d: %s: %d' % (a, r[1], r[0])
 
-				pool.close()
-				print 'waiting for processes to exit...'
-				pool.join()
-				print 'finished.'
+		pool.close()
+		print 'waiting for processes to exit...'
+		pool.join()
+		print 'finished.'
