c0761a32c7017900015286de3b673beed290e73c
Robert Muil <Robert_Muil@mckinsey.com>
2019-07-10 15:11:55 +0100

    made script work

--- bin/mac_disable_chrome_swipe.bash
+++ bin/mac_disable_chrome_swipe.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
 defaults write com.google.Chrome.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
